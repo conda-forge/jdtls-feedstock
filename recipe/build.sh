0cb635a52b381417b440a4fb902053bbe3a2e751
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-10 13:34:09 -0800

    update to 1.54.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 touch ${BUILD_PREFIX}/lib/jvm/release
 
+cp ${RECIPE_DIR}/0001-use-release-repository.patch ${SRC_DIR}
+patch -p0 0001-use-release-repository.patch
+
 # Build with maven
 ./mvnw install -T 1 -DskipTests=true -Dmaven.local.repo=$SRC_DIR
 
